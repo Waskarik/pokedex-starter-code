0fcb7c346ef5728130732ddf06210b518296ba3a
Leanderson Costa <exmoser@gmail.com>
2026-08-18 12:09:42 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState} from "react"
 import { Link } from "react-router-dom"
 
 function Sidebar() {
+
+  const [pokeList, setPokeList] = useState(null)
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then((response) => {
+      console.log(response)
+      return response.json()
+    })
+    .then((response)=>{
+      console.log(response.results)
+      setPokeList(response.results)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }, [])
+
+ // if(!pokeList) return <h3> Loading</h3>
+
   return (
     <nav className="sidebar">
 
       <h4>Pokemon</h4>
-      
+      {pokeList ? pokeList.map((pokemon, i) => {
+        return <Link key = {i} to={`/pokemon-details/${pokemon.name}`}>{pokemon.name}</Link>
+      }): <h3> Loading</h3>}
       {/* example of 3 links */}
       <Link to={"/pokemon-details/bulbasaur"}>bulbasaur</Link>
       <Link to={"/pokemon-details/charmander"}>charmander</Link>
--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -1,21 +1,51 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams , useNavigate } from "react-router-dom";
+
 function PokemonDetails() {
+  const params = useParams()
+  const navigate = useNavigate()
+
+  const [pokemon, setPokemon] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, [params.pokemonName]);
+  const getData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${params.pokemonName}`,
+      );
+      console.log(response);
+      setPokemon(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      navigate("/error")
+    }
+  };
+  if (isLoading) return <h2>is loading</h2>;
   return (
     <div>
-      
       <h2>Pokemon Details</h2>
 
-      <h1>NAME</h1>
+      <h1>{pokemon.name}</h1>
 
-      <img src="POKEMON_IMAGE_URL" alt="pokemon" height="150px" />
+      <img
+        src={pokemon.sprites.other.dream_world.front_default}
+        alt="pokemon"
+        height="150px"
+      />
 
-      <h4>Height: HEIGHT m</h4>
-      <h4>Weight: WEIGHT kg</h4>
+      <h4>Height: {pokemon.height} m</h4>
+      <h4>Weight: {pokemon.weight} kg</h4>
 
       <h4>Type</h4>
-
-      <p>POKEMON_TYPE_1</p>
-      <p>POKEMON_TYPE_2</p>
-
+      {pokemon.types.map((type, i) => {
+        return <p key={i}>{type.type.name}</p>;
+      })}
     </div>
   );
 }
